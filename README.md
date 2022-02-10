f594e23e2f3fefdf761fe4306d69851dc69e12b4
unknown <tiddy11@sina.com>
2022-02-11 03:51:20 +0800

    update

--- README.md
+++
@@ -1,3 +0,0 @@
-# FlaskApp
-# FlaskApp
-# FlaskApp
--- code/app/config.py
+++
@@ -1,6 +0,0 @@
-db_config = {'user': 'ece1779', 
-             'password': 'secret',
-             'host': '127.0.0.1',
-             'database': 'imagedb'}
-
-UPLOAD_FOLDER = "static/images/upload"
